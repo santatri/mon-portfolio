eb02d0de304a47f5609f746e9eb136e594b8b3dc
santatri <santatrinianafeno01@gmail.com>
2025-04-13 10:46:32 +0200

    Nettoyage avant rebuild 2

--- mon-portfolio/src/App.jsx
+++ mon-portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom'; // Supprimez BrowserRouter d'ici
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -8,18 +8,16 @@ import Skills from './components/Skills';
 
 export default function App() {
   return (
-    <Router basename="/">
-      <div className="font-sans text-gray-800">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="font-sans text-gray-800">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
+    </div>
   );
 }
