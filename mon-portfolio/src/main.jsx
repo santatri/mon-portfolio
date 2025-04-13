72481e778c3d603eec828daca21b3715bb4e7eeb
santatri <santatrinianafeno01@gmail.com>
2025-04-13 11:09:45 +0200

    Nettoyage avant rebuild vercel

--- mon-portfolio/src/main.jsx
+++ mon-portfolio/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // <-- Ajoutez cette ligne
 import App from './App';
 import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/"> {/* <-- Enveloppez votre App avec BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
